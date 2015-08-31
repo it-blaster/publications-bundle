e10becd2d009f0be5d5a97b8e3138440b5abb56c
Valery Vlasov <valeri-vlasov@yandex.ru>
2015-08-31 13:14:53 +0500

    Update Publication.php
    Метод проверяет флаг публикации и дату публикации и на основании этого возвращает результат видимости публикации для конечного пользователя.

--- Model/Publication.php
+++ Model/Publication.php
@@ -6,4 +6,19 @@ use Fenrizbes\PublicationsBundle\Model\om\BasePublication;
 
 class Publication extends BasePublication
 {
+  
+    /**
+     * Проверяем видимость новости для посетителей
+     * @return bool
+     * @throws \PropelException
+     */
+    public function getIsVisible()
+    {
+        if ($this->getIsPublished() && $this->getCreatedAt() <= new \DateTime('now')) {
+            return true;
+        }
+
+        return false;
+    }  
+  
 }
